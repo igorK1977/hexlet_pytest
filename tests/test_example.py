6ac4dffc8e40e45db42ca4f28cea327e836fd827
igor komrachkov <ikomrachkov@mail.ru>
2025-08-22 11:28:11 +0300

    added test for test_example.py

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,4 +1,13 @@
 from hexlet_pytest.example import reverse
+from pathlib import Path
+
+
+def get_test_data_path(filename):
+    return Path(__file__).parent / "test_data" / filename
+
+
+def read_file(filename):
+    return get_test_data_path(filename).read_text()
 
 
 def test_reverse():
@@ -7,3 +16,11 @@ def test_reverse():
 
 def test_reverse_for_empty_string():
     assert reverse("") == ""
+
+
+def test_reverse_for_long_string():
+    src_string = read_file("src_string.txt")
+    expected = read_file("res_string.txt")
+    actual = reverse(src_string)
+
+    assert actual == expected
